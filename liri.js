3df4549d17b28fbbd07f98df3cf1f730c5f22099
Ray Riney <rayfordriney@gmail.com>
2019-11-21 13:24:12 -0800

    "added js and text files"

---
+++ liri.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+var request = require("request");
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
+var fs = require("fs");
+var moment = rerquire("moment");
+
+var command = process.argv[2];
+
+if (command ==="concert-this") {
+ 
+  
+    var artist = process.argv[3];
+
+
+    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id = codingbootcamp", function (error,response,body) {
+
+    if (!error && response.statusCode === 200) {
+
+        console.log("------------------------";
+        console.log("Venue:"+ JSON.parse(body[0].venue.name);
+        console.log("location:" + JSON.parse(body[0].venue.city + "" + JSON.parse(body)[0].venue.region);
+        console.log("Date: + moment(JSON.parse(body)[0].datetime).format("mm/dd/yy));
+        console.log("-----------------------");
+
+    
+    }
+});       
+
+
+} else if (command === "spotify-this-song") {
+    var song = process.argv[3]
+
+
+    if (song   === undefined) {
+        song = "The Sign";
+
+    }
+
+
+spotify.search({
+    type:"track",
+    query: song
+}, function (err, data) {
+    if (err) {
+        return console.log("Error occured: " + err);
+
+    }
+
+
+console.log("------------------------")
+console.log("")
+
+})
