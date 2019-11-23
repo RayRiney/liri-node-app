dffd67d4f7bd8afaf74f1731da76cdd11406de46
Ray Riney <rayfordriney@gmail.com>
2019-11-23 04:31:31 -0800

    Gitacora saved 1 changed files

--- liri.js
+++ liri.js
@@ -86,33 +86,103 @@ if (command === "concert-this") {
 
             } else if (command === "do what it says") {
                 fs.readFile("randon.txt", "utf8", function(error, data) {
-                            if (error) {
-                                return console.log(error);
+                    if (error) {
+                        return console.log(error);
 
 
 
 
+                    }
+
+                    console.log(data);
+                    var dataArr = data.split(",")
+                    console.log(dataArr);
+
+                    command = dataArr[0];
+                    whatTocommand = dataArr[1];
+
+                    if (command === "concert-this") {
+                        var artist = whatTocommand
+
+
+
+                        request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response, body) {
+
+                            if (!error && response.statusCode === 200) {
+                                console.log("-------------------------------------");
+                                console.log("Venue: " + JSON.parse(body)[0].venue.name);
+                                console.log("Location: " + JSON.parse(body)[0].venue.city + " " + JSON.parse(body)[0].venue.region);
+                                console.log("Date: " + moment(JSON.parse(body)[0].datetime).format("MM/DD/YYYY"));
+                                console.log("-------------------------------------");
                             }
-
-                            console.log(data);
-                            var dataArr = data.split(",")
-                            console.log(dataArr);
-
-                            command = dataArr[0];
-                            whatTocommand = dataArr[1];
-
-                            if (command === "concert-this") {
-                                var artist = whatTocommand
+                        });
 
 
+                    } else if (command === "spotify-this-song") {
+                        var song = whatToCommand;
 
-                                request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response, body) {
+                        if (song === undefined) {
+                            song = "The Sign";
+                        }
 
-                                    if (!error && response.statusCode === 200) {
-                                        console.log("-------------------------------------");
-                                        console.log("Venue: " + JSON.parse(body)[0].venue.name);
-                                        console.log("Location: " + JSON.parse(body)[0].venue.city + " " + JSON.parse(body)[0].venue.region);
-                                        console.log("Date: " + moment(JSON.parse(body)[0].datetime).format("MM/DD/YYYY"));
-                                        console.log("-------------------------------------");
-                                    }
-                                });
+                        spotify.search({
+                            type: "track",
+                            query: song
+                        }, function(err, data) {
+                            if (err) {
+                                return console.log("Error occured: " + err);
+                            }
+                            console.log(data.tracks.items[0].album[0]);
+                            // console.log("-------------------------------------");
+                            // console.log("Command: ", command, ", Song Name: ", song);
+                            // console.log("Artist: " + data);
+                            // console.log("Song Name: " + data);
+                            // console.log("Preview Link: " + data);
+                            // console.log("Album: " + data);
+                            // console.log("-------------------------------------");
+                        });
+
+
+
+                    } else if (command === "movie-this") {
+
+                        var movie = whatToCommand;
+
+                        if (movie === undefined) {
+                            movie = "Mr. Nobody";
+                        }
+
+                        request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+                            if (!error && response.statusCode === 200) {
+
+
+                                console.log("-------------------------------------");
+                                console.log("Title: " + JSON.parse(body).Title);
+                                console.log("Year Released: " + JSON.parse(body).Year);
+                                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                                console.log("Country Produced: " + JSON.parse(body).Country);
+                                console.log("Language: " + JSON.parse(body).Language);
+                                console.log("Plot: " + JSON.parse(body).Plot);
+                                console.log("Actors: " + JSON.parse(body).Actors);
+                                console.log("-------------------------------------");
+                            }
+                        });
+
+
+
+                    } else {
+                        console.log("Command Error");
+                    }
+
+                    console.log("-------------------------------------");
+                    console.log("Command: ", command);
+                    console.log("-------------------------------------");
+                });
+
+
+
+            } else {
+                console.log("Command Error");
+            }
