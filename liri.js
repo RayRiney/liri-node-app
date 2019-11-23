b3c2d355a9f48c6b35f944bea853e461c6d7e514
Ray Riney <rayfordriney@gmail.com>
2019-11-23 03:11:01 -0800

    Gitacora saved 1 changed files

--- liri.js
+++ liri.js
@@ -79,4 +79,4 @@ if (command === "concert-this") {
                         console.log("-------------------------------------");
                     }
                 });
-            } else if (command === "do what it") says
+            } else if (command === "do what it says") says
