e6d55da79700b83c78505e21f859e9e16d4c9082
Ray Riney <rayfordriney@gmail.com>
2019-11-23 02:55:01 -0800

    Gitacora saved 1 changed files

--- liri.js
+++ liri.js
@@ -7,48 +7,47 @@ var moment = rerquire("moment");
 
 var command = process.argv[2];
 
-if (command ==="concert-this") {
- 
-  
+if (command === "concert-this") {
+
+
     var artist = process.argv[3];
 
 
-    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id = codingbootcamp", function (error,response,body) {
+    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id = codingbootcamp", function(error, response, body) {
 
-    if (!error && response.statusCode === 200) {
+                if (!error && response.statusCode === 200) {
 
-        console.log("------------------------";
-        console.log("Venue:"+ JSON.parse(body[0].venue.name);
-        console.log("location:" + JSON.parse(body[0].venue.city + "" + JSON.parse(body)[0].venue.region);
-        console.log("Date: + moment(JSON.parse(body)[0].datetime).format("mm/dd/yy));
-        console.log("-----------------------");
-
-    
-    }
-});       
+                    console.log("------------------------"; console.log("Venue:" + JSON.parse(body[0].venue.name); console.log("location:" + JSON.parse(body[0].venue.city + "" + JSON.parse(body)[0].venue.region); console.log("Date: + moment(JSON.parse(body)[0].datetime).format("
+                                mm / dd / yy)); console.log("-----------------------");
 
 
-} else if (command === "spotify-this-song") {
-    var song = process.argv[3]
+                        }
+                    });
+
+                lse
+                if (command === "spotify-this-song") {
+                    var song = process.argv[3]
 
 
-    if (song   === undefined) {
-        song = "The Sign";
+                    if (song ===
+                    }
+                    e undefined) {
+                    song = "The Sign";
 
-    }
-
-
-spotify.search({
-    type:"track",
-    query: song
-}, function (err, data) {
-    if (err) {
-        return console.log("Error occured: " + err);
-
-    }
+                }
 
 
-console.log("------------------------")
-console.log("")
+                spotify.search({
+                    type: "track",
+                    query: song
+                }, function(err, data) {
+                    if (err) {
+                        return console.log("Error occured: " + err);
 
-})
+                    }
+
+
+                    console.log("------------------------")
+                    console.log("")
+
+                })
